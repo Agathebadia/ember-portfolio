2bdd3359beded8a0a0c628895546f0b2ed542964
Agathe Badia <ab@honeypot.io>
2020-07-04 19:33:39 +0200

    Toggle action is adding a class on the elements

---
+++ app/components/nav-bar.js
@@ -0,0 +1,6 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+
+export default class NavBarComponent extends Component {
+}
