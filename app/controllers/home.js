1b99cc85c7a651b3187b8eb323608323016875de
Agathe Badia <ab@honeypot.io>
2020-07-04 21:02:03 +0200

    Start redesign color scheme on the banner

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,14 +3,9 @@ import { action } from '@ember/object'
 
 export default class HomeController extends Controller {
   isToggled = false;
-  // constructor() {
-  //   super(...arguments);
-  //   this.isToggled = false;
-  // }
+
   @action
   toggleColor() {
-    // this.isToggled = !this.isToggled;
     this.set('isToggled', !this.isToggled)
-    // $('.banner').toggleClass('purple-color-scheme', this.isToggled);
   }
 }
