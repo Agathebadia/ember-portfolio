3fff20b66abff641b5631cefd4f7aa8d124b41c3
Agathe Badia <ab@honeypot.io>
2022-01-08 12:03:28 +0100

    Create content component and refactor home page

--- app/routes/home.js
+++ app/routes/home.js
@@ -39,7 +39,91 @@ export default class HomeRoute extends Route {
           "aria-label": "Visit Tooli Project",
           href: "https://tooli-capmarvel.herokuapp.com/"
         },
-      ]
+      ],
+      content: [
+        {
+          title: "Agathe's Diary - Podcast & Tech Stories",
+          alt: "Rubber duck with the title of the podcast, Agathe's Diary",
+          height: '230',
+          src: "assets/images/Agathe-s-Diary.jpg",
+          "aria-label": "Visit Agathe's Diary Podcast",
+          href: "https://www.buzzsprout.com/1248158"
+        },
+                {
+          title: "Untold Developer Stories - Audio podcast",
+          alt: "Glitchy background with a title with a black round centered design 'Untold developer stories",
+          height: '230',
+          src: "assets/images/untold-stories.jpg",
+          "aria-label": "Visit Untold Developer Stories Podcast",
+          href: "https://www.buzzsprout.com/979024"
+        },
+                {
+          title: "Untold Developer Stories - Youtube Podcast",
+          alt: "Screenshot of Agathe on a Youtube episode of Untold dev stories",
+          src: "assets/images/youtube.png",
+          "aria-label": "Visit Untold Developer Stories, Youtube podcast",
+          height: '230',
+          href: "https://www.youtube.com/playlist?list=PLtEPUaeDclkvfFlePNrYqYUrtDhEB6g-I"
+        },
+                {
+          title: "Contributor on cult, Honeypot's Community Platform",
+          alt: "Agathe and another student, chatting and looking at a laptop",
+          height: '230',
+          src: "assets/images/coding-bootcamp.jpg",
+          "aria-label": "Check out Agathe's contributions on Honeypot's community platform",
+          href: "https://cult.honeypot.io/contributors/agathe-badia"
+        },
+                {
+          title: "Accessible & Inclusive Blog",
+          alt: "Agathe sitting at a desk, on her computer",
+          src: "assets/images/journey-to-dev.jpg",
+          height: '230',
+          "aria-label": "Visit Agathe's Accessible Blog",
+          href: "https://inclusive-blog-post.netlify.app/"
+        }
+      ],
+      media: [
+        {
+          title: "My Journey to Tech on CodeStory, with Nathaniel Idahosa",
+          alt: "The codestories podcast number 20, in light purple on a black background",
+          height: '220',
+          src: "assets/images/codestory.png",
+          "aria-label": "Check out the podcast on Youtube",
+          href: "https://www.youtube.com/watch?v=Sjd60oa5Dt8"
+        },
+                {
+          title: "Untold Developer Stories - Audio podcast",
+          alt: "Glitchy background with a title with a black round centered design 'Untold developer stories",
+          height: '230',
+          src: "assets/images/untold-stories.jpg",
+          "aria-label": "Visit Untold Developer Stories Podcast",
+          href: "https://www.buzzsprout.com/979024"
+        },
+                {
+          title: "Untold Developer Stories - Youtube Podcast",
+          alt: "Screenshot of Agathe on a Youtube episode of Untold dev stories",
+          src: "assets/images/youtube.png",
+          "aria-label": "Visit Untold Developer Stories, Youtube podcast",
+          height: '230',
+          href: "https://www.youtube.com/playlist?list=PLtEPUaeDclkvfFlePNrYqYUrtDhEB6g-I"
+        },
+                {
+          title: "Contributor on cult, Honeypot's Community Platform",
+          alt: "Agathe and another student, chatting and looking at a laptop",
+          height: '230',
+          src: "assets/images/coding-bootcamp.jpg",
+          "aria-label": "Check out Agathe's contributions on Honeypot's community platform",
+          href: "https://cult.honeypot.io/contributors/agathe-badia"
+        },
+                {
+          title: "Accessible & Inclusive Blog",
+          alt: "Agathe sitting at a desk, on her computer",
+          src: "assets/images/journey-to-dev.jpg",
+          height: '230',
+          "aria-label": "Visit Agathe's Accessible Blog",
+          href: "https://inclusive-blog-post.netlify.app/"
+        }
+      ],
     };
   }
 }
